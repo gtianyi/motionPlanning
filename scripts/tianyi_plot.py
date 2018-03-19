4dbae19dc335d38e1167ad082943e8604cb3769e
Tianyi Gu <gu@cs.unh.edu>
2018-03-19 14:55:55 -0400

    tianyi-plot

---
+++ scripts/tianyi_plot.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from math import * 
+
+
+df4 = pd.read_json('data-hnb-kcar&dcar-nog.json')
+df2 = pd.read_json('data-hnb-h&q&b-nog.json')
+df3 = pd.read_json('data-h&q&b-nog.json')
+df = pd.read_json('data-dcar&kcar-nog.json')
+df = df.append(df2)
+df = df.append(df4)
+df = df.append(df3)
+print(df.columns)
+df = df.loc[:, ['Domain', 'Algorithm', 'time', 'EnvironmentName']]
+
+fig = plt.figure()
+count = df.Domain.unique().size
+c = int(ceil(sqrt(count)))
+r = int(ceil(float(count) / float(c)))
+#sns.set(font_scale = 0.8)
+for i, d in enumerate(df.Domain.unique()):
+    axcurr = fig.add_subplot(r, c, i + 1)
+    box_plot = sns.boxplot(x='EnvironmentName', y='time', \
+                           hue='Algorithm', ax = axcurr,\
+                           data=df.loc[df['Domain'] == d], \
+                           showmeans = True, meanline = True, \
+                           notch = True, palette = "Set3")
+    box_plot.set_title(d)
+    box_plot.set_xlabel('')
+    box_plot.set_xticklabels(box_plot.get_xticklabels(), rotation = 17)
+    if i + 1 == count:
+        box_plot.legend(loc =2,  bbox_to_anchor = (1.05, 1))
+    else:
+        box_plot.legend_.remove()
+
+plt.tight_layout()
+plt.savefig('beast-bonus-halton-nog.eps')
+# sns.despine(offset=10, trim=True)
