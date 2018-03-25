cd74e4e421fdcc550e58495a197ce76cca857751
Bence Cserna <bence@cserna.net>
2018-03-25 16:41:22 -0400

    Fix problem definition initialization.

--- planners/beastplanner.hpp
+++ planners/beastplanner.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
+#include "ompl/base/goals/GoalSampleableRegion.h"
 #include "ompl/control/planners/PlannerIncludes.h"
 #include "ompl/datastructures/NearestNeighbors.h"
-#include "ompl/base/goals/GoalSampleableRegion.h"
 #include "ompl/tools/config/SelfConfig.h"
 
 #include "../structs/filemap.hpp"
 
-#include "../samplers/beastsampler_dstar.hpp"
 #include "../samplers/beastsampler_dijkstra.hpp"
+#include "../samplers/beastsampler_dstar.hpp"
 #include "../samplers/beastsampler_dstarNewBonus.hpp"
 #include "../samplers/beastsampler_dstarNoGeometricTest.hpp"
 #include "../samplers/integrated_beast.hpp"
@@ -21,37 +21,55 @@ namespace control {
 
 template <typename Sampler>
 class BeastPlanner : public ompl::control::RRT {
-  public:
-
+public:
     /** \brief Constructor */
-    BeastPlanner(const SpaceInformationPtr &si, const FileMap &params) :
-            ompl::control::RRT(si), newsampler_(NULL), params(params), 
-            sampler(
-                (ompl::base::SpaceInformation *)siC_,
-                pdef_->getStartState(0),
-                pdef_->getGoal(),
-                dynamic_cast<base::GoalSampleableRegion *>(pdef_->getGoal().get()), params) {
-
+    BeastPlanner(const SpaceInformationPtr& si, const FileMap& params)
+            : ompl::control::RRT(si), sampler(nullptr), params(params) {
         whichSearch = params.stringVal("WhichSearch");
 
         std::string plannerName = "BeastPlanner_" + whichSearch;
         setName(plannerName);
 
-        Planner::declareParam<double>("state_radius", this, &BeastPlanner::ignoreSetterDouble, &BeastPlanner::getStateRadius);
-        Planner::declareParam<unsigned int>("prm_size", this, &BeastPlanner::ignoreSetterUnsigedInt, &BeastPlanner::getPRMSize);
-        Planner::declareParam<unsigned int>("num_prm_edges", this, &BeastPlanner::ignoreSetterUnsigedInt, &BeastPlanner::getNumPRMEdges);
+        Planner::declareParam<double>("state_radius",
+                this,
+                &BeastPlanner::ignoreSetterDouble,
+                &BeastPlanner::getStateRadius);
+        Planner::declareParam<unsigned int>("prm_size",
+                this,
+                &BeastPlanner::ignoreSetterUnsigedInt,
+                &BeastPlanner::getPRMSize);
+        Planner::declareParam<unsigned int>("num_prm_edges",
+                this,
+                &BeastPlanner::ignoreSetterUnsigedInt,
+                &BeastPlanner::getNumPRMEdges);
 
-        Planner::declareParam<double>("valid_edge_distribution_alpha", this, &BeastPlanner::ignoreSetterDouble, &BeastPlanner::getValidEdgeDistributionAlpha);
-        Planner::declareParam<double>("valid_edge_distribution_beta", this, &BeastPlanner::ignoreSetterDouble, &BeastPlanner::getValidEdgeDistributionBeta);
-        Planner::declareParam<double>("invalid_edge_distribution_alpha", this, &BeastPlanner::ignoreSetterDouble, &BeastPlanner::getInvalidEdgeDistributionAlpha);
-        Planner::declareParam<double>("invalid_edge_distribution_beta", this, &BeastPlanner::ignoreSetterDouble, &BeastPlanner::getInvalidEdgeDistributionBeta);
+        Planner::declareParam<double>("valid_edge_distribution_alpha",
+                this,
+                &BeastPlanner::ignoreSetterDouble,
+                &BeastPlanner::getValidEdgeDistributionAlpha);
+        Planner::declareParam<double>("valid_edge_distribution_beta",
+                this,
+                &BeastPlanner::ignoreSetterDouble,
+                &BeastPlanner::getValidEdgeDistributionBeta);
+        Planner::declareParam<double>("invalid_edge_distribution_alpha",
+                this,
+                &BeastPlanner::ignoreSetterDouble,
+                &BeastPlanner::getInvalidEdgeDistributionAlpha);
+        Planner::declareParam<double>("invalid_edge_distribution_beta",
+                this,
+                &BeastPlanner::ignoreSetterDouble,
+                &BeastPlanner::getInvalidEdgeDistributionBeta);
 
-        //Obviously this isn't really a parameter but I have no idea how else to get it into the output file through the benchmarker
-        Planner::declareParam<double>("sampler_initialization_time", this, &BeastPlanner::ignoreSetterDouble, &BeastPlanner::getSamplerInitializationTime);
+        // Obviously this isn't really a parameter but I have no idea how else
+        // to get it into the output file through the benchmarker
+        Planner::declareParam<double>("sampler_initialization_time",
+                this,
+                &BeastPlanner::ignoreSetterDouble,
+                &BeastPlanner::getSamplerInitializationTime);
 
         auto start = clock();
 
-        newsampler_->initialize();
+        sampler->initialize();
 
         samplerInitializationTime = (double)(clock() - start) / CLOCKS_PER_SEC;
     }
@@ -61,12 +79,8 @@ class BeastPlanner : public ompl::control::RRT {
     void ignoreSetterDouble(double) const {}
     void ignoreSetterUnsigedInt(unsigned int) const {}
 
-    double getStateRadius() const {
-        return params.doubleVal("StateRadius");
-    }
-    unsigned int getPRMSize() const {
-        return params.integerVal("PRMSize");
-    }
+    double getStateRadius() const { return params.doubleVal("StateRadius"); }
+    unsigned int getPRMSize() const { return params.integerVal("PRMSize"); }
     unsigned int getNumPRMEdges() const {
         return params.integerVal("NumEdges");
     }
@@ -86,96 +100,123 @@ class BeastPlanner : public ompl::control::RRT {
         return samplerInitializationTime;
     }
 
-    /** \brief Continue solving for some amount of time. Return true if solution was found. */
-    virtual base::PlannerStatus solve(const base::PlannerTerminationCondition &ptc) {
-        checkValidity();
-        base::Goal                   *goal = pdef_->getGoal().get();
-        base::GoalSampleableRegion *goal_s = dynamic_cast<base::GoalSampleableRegion *>(goal);
+    virtual void setProblemDefinition(
+            const ompl::base::ProblemDefinitionPtr& pdef) override {
+        ompl::control::RRT::setProblemDefinition(pdef);
+        sampler =
+                std::unique_ptr<Sampler>(new Sampler(
+                    (ompl::base::SpaceInformation*)siC_,
+                        pdef_->getStartState(0),
+                        pdef_->getGoal(),
+                        dynamic_cast<base::GoalSampleableRegion*>(
+                                pdef_->getGoal().get()),
+                        params));
+    }
 
-        while(const base::State *st = pis_.nextStart()) {
-            Motion *motion = new Motion(siC_);
+    /** \brief Continue solving for some amount of time. Return true if solution
+     * was found. */
+    virtual base::PlannerStatus solve(
+            const base::PlannerTerminationCondition& ptc) {
+        checkValidity();
+        base::Goal* goal = pdef_->getGoal().get();
+        base::GoalSampleableRegion* goal_s =
+                dynamic_cast<base::GoalSampleableRegion*>(goal);
+
+        while (const base::State* st = pis_.nextStart()) {
+            Motion* motion = new Motion(siC_);
             si_->copyState(motion->state, st);
             siC_->nullControl(motion->control);
             nn_->add(motion);
         }
 
-        if(nn_->size() == 0) {
-            OMPL_ERROR("%s: There are no valid initial states!", getName().c_str());
+        if (nn_->size() == 0) {
+            OMPL_ERROR("%s: There are no valid initial states!",
+                    getName().c_str());
             return base::PlannerStatus::INVALID_START;
         }
 
-        if(!controlSampler_)
+        if (!controlSampler_)
             controlSampler_ = siC_->allocDirectedControlSampler();
 
-        OMPL_INFORM("%s: Starting planning with %u states already in datastructure", getName().c_str(), nn_->size());
+        OMPL_INFORM(
+                "%s: Starting planning with %u states already in datastructure",
+                getName().c_str(),
+                nn_->size());
 
-        Motion *solution  = NULL;
-        Motion *approxsol = NULL;
-        double  approxdif = std::numeric_limits<double>::infinity();
+        Motion* solution = nullptr;
+        Motion* approxsol = nullptr;
+        double approxdif = std::numeric_limits<double>::infinity();
 
-        Motion      *rmotion = new Motion(siC_);
-        base::State  *rstate = rmotion->state;
-        Control       *rctrl = rmotion->control;
-        base::State  *xstate = si_->allocState();
+        Motion* rmotion = new Motion(siC_);
+        base::State* rstate = rmotion->state;
+        Control* rctrl = rmotion->control;
+        base::State* xstate = si_->allocState();
 
-        Motion *resusableMotion = new Motion(siC_);
+        Motion* resusableMotion = new Motion(siC_);
         int propNum = 0;
-        while(ptc == false) {
+        while (ptc == false) {
             propNum++;
-            Motion *nmotion = NULL;
+            Motion* nmotion = nullptr;
 
             /* sample random state (with goal biasing) */
-            // if(goal_s && rng_.uniform01() < goalBias_ && goal_s->canSample()) {
+            // if(goal_s && rng_.uniform01() < goalBias_ && goal_s->canSample())
+            // {
             // 	goal_s->sampleGoal(rstate);
 
             // 	/* find closest state in the tree */
             // 	nmotion = nn_->nearest(rmotion);
             // }
             // else {
-            newsampler_->sample(resusableMotion->state, rstate);
+            sampler->sample(resusableMotion->state, rstate);
 
             // std::cout << "from state ============ " << std::endl;
-            // auto s = resusableMotion->state->as<ompl::base::CompoundStateSpace::StateType>()->as<ompl::base::SE3StateSpace::StateType>(0);
+            // auto s =
+            // resusableMotion->state->as<ompl::base::CompoundStateSpace::StateType>()->as<ompl::base::SE3StateSpace::StateType>(0);
             // std::cout << s->getX() << " " << s->getY() << std::endl;
             // std::cout << "rstate ============ " << std::endl;
-            // s = rstate->as<ompl::base::CompoundStateSpace::StateType>()->as<ompl::base::SE3StateSpace::StateType>(0);
+            // s =
+            // rstate->as<ompl::base::CompoundStateSpace::StateType>()->as<ompl::base::SE3StateSpace::StateType>(0);
             // std::cout << s->getX() << " " << s->getY() << std::endl;
-            
+
             /* find closest state in the tree */
             nmotion = nn_->nearest(rmotion);
-            // std::cout << "nearest state ============ " << std::endl;
-            // s = nmotion->state->as<ompl::base::CompoundStateSpace::StateType>()->as<ompl::base::SE3StateSpace::StateType>(0);
-            // std::cout << s->getX() << " " << s->getY() << std::endl;
-            // }
+// std::cout << "nearest state ============ " << std::endl;
+// s =
+// nmotion->state->as<ompl::base::CompoundStateSpace::StateType>()->as<ompl::base::SE3StateSpace::StateType>(0);
+// std::cout << s->getX() << " " << s->getY() << std::endl;
+// }
 
 #ifdef STREAM_GRAPHICS
             streamPoint(rmotion->state, 0, 1, 0, 1);
 #endif
 
-            /* sample a random control that attempts to go towards the random state, and also sample a control duration */
-            unsigned int cd = controlSampler_->sampleTo(rctrl, nmotion->control, nmotion->state, rmotion->state);
+            /* sample a random control that attempts to go towards the random
+             * state, and also sample a control duration */
+            unsigned int cd = controlSampler_->sampleTo(
+                    rctrl, nmotion->control, nmotion->state, rmotion->state);
 
-            if(addIntermediateStates_) {
+            if (addIntermediateStates_) {
                 // this code is contributed by Jennifer Barry
-                std::vector<base::State *> pstates;
-                cd = siC_->propagateWhileValid(nmotion->state, rctrl, cd, pstates, true);
+                std::vector<base::State*> pstates;
+                cd = siC_->propagateWhileValid(
+                        nmotion->state, rctrl, cd, pstates, true);
 
-                if(cd >= siC_->getMinControlDuration()) {
-                    Motion *lastmotion = nmotion;
+                if (cd >= siC_->getMinControlDuration()) {
+                    Motion* lastmotion = nmotion;
                     bool solved = false;
                     size_t p = 0;
-                    for(; p < pstates.size(); ++p) {
+                    for (; p < pstates.size(); ++p) {
                         /* create a motion */
-                        Motion *motion = new Motion();
+                        Motion* motion = new Motion();
                         motion->state = pstates[p];
 
-                        newsampler_->reached(pstates[p]);
+                        sampler->reached(pstates[p]);
 
 #ifdef STREAM_GRAPHICS
                         streamPoint(pstates[p], 1, 0, 0, 1);
 #endif
 
-                        //we need multiple copies of rctrl
+                        // we need multiple copies of rctrl
                         motion->control = siC_->allocControl();
                         siC_->copyControl(motion->control, rctrl);
                         motion->steps = 1;
@@ -184,36 +225,36 @@ class BeastPlanner : public ompl::control::RRT {
                         nn_->add(motion);
                         double dist = 0.0;
                         solved = goal->isSatisfied(motion->state, &dist);
-                        if(solved) {
+                        if (solved) {
                             approxdif = dist;
                             solution = motion;
                             break;
                         }
-                        if(dist < approxdif) {
+                        if (dist < approxdif) {
                             approxdif = dist;
                             approxsol = motion;
                         }
                     }
 
-                    //free any states after we hit the goal
-                    while(++p < pstates.size())
+                    // free any states after we hit the goal
+                    while (++p < pstates.size())
                         si_->freeState(pstates[p]);
-                    if(solved)
+                    if (solved)
                         break;
                 } else
-                    for(size_t p = 0 ; p < pstates.size(); ++p)
+                    for (size_t p = 0; p < pstates.size(); ++p)
                         si_->freeState(pstates[p]);
             } else {
-                if(cd >= siC_->getMinControlDuration()) {
+                if (cd >= siC_->getMinControlDuration()) {
                     /* create a motion */
-                    Motion *motion = new Motion(siC_);
+                    Motion* motion = new Motion(siC_);
 
                     si_->copyState(motion->state, rmotion->state);
                     siC_->copyControl(motion->control, rctrl);
                     motion->steps = cd;
                     motion->parent = nmotion;
 
-                    newsampler_->reached(motion->state);
+                    sampler->reached(motion->state);
 
 #ifdef STREAM_GRAPHICS
                     streamPoint(nmotion->state, 1, 0, 0, 1);
@@ -223,50 +264,53 @@ class BeastPlanner : public ompl::control::RRT {
                     nn_->add(motion);
                     double dist = 0.0;
                     bool solv = goal->isSatisfied(motion->state, &dist);
-                    if(solv) {
+                    if (solv) {
                         approxdif = dist;
                         solution = motion;
                         break;
                     }
-                    if(dist < approxdif) {
+                    if (dist < approxdif) {
                         approxdif = dist;
                         approxsol = motion;
                     }
                 }
             }
         }
-        std::cout << "propNUM: "<< propNum << std::endl;
+        std::cout << "propNUM: " << propNum << std::endl;
         bool solved = false;
         bool approximate = false;
-        if(solution == NULL) {
+        if (solution == nullptr) {
             solution = approxsol;
             approximate = true;
         }
 
-        if(solution != NULL) {
+        if (solution != nullptr) {
             lastGoalMotion_ = solution;
 
             /* construct the solution path */
-            std::vector<Motion *> mpath;
-            while(solution != NULL) {
+            std::vector<Motion*> mpath;
+            while (solution != nullptr) {
                 mpath.push_back(solution);
                 solution = solution->parent;
             }
 
             /* set the solution path */
-            PathControl *path = new PathControl(si_);
-            for(int i = mpath.size() - 1 ; i >= 0 ; --i)
-                if(mpath[i]->parent)
-                    path->append(mpath[i]->state, mpath[i]->control, mpath[i]->steps * siC_->getPropagationStepSize());
+            PathControl* path = new PathControl(si_);
+            for (int i = mpath.size() - 1; i >= 0; --i)
+                if (mpath[i]->parent)
+                    path->append(mpath[i]->state,
+                            mpath[i]->control,
+                            mpath[i]->steps * siC_->getPropagationStepSize());
                 else
                     path->append(mpath[i]->state);
             solved = true;
-            pdef_->addSolutionPath(base::PathPtr(path), approximate, approxdif, getName());
+            pdef_->addSolutionPath(
+                    base::PathPtr(path), approximate, approxdif, getName());
         }
 
-        if(rmotion->state)
+        if (rmotion->state)
             si_->freeState(rmotion->state);
-        if(rmotion->control)
+        if (rmotion->control)
             siC_->freeControl(rmotion->control);
         delete rmotion;
         si_->freeState(xstate);
@@ -278,19 +322,13 @@ class BeastPlanner : public ompl::control::RRT {
 
     virtual void clear() {
         RRT::clear();
-        // delete newsampler_;
-        // newsampler_ = NULL;
     }
 
-  protected:
-
-    Sampler sampler;
-    ompl::base::BeastSamplerBase *newsampler_;
-    const FileMap &params;
+protected:
+    std::unique_ptr<Sampler> sampler;
+    const FileMap& params;
     std::string whichSearch;
     double samplerInitializationTime = 0;
 };
-
 }
-
 }
