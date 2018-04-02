0d3b8910dc204590feba3f180d14a3ece1c33555
Tianyi Gu <gu@cs.unh.edu>
2018-04-02 18:40:39 -0400

    quick experiement setting

--- scripts/configuration_generator.py
+++ scripts/configuration_generator.py
@@ -1,7 +1,8 @@
 def forest_environment():
     base = 'EnvironmentMesh ? forest.dae\n' \
            '{start_goal_pair}\n' \
-           'EnvironmentBounds ? -30 30 -30 30\n'
+           'EnvironmentBounds ? -30 30 -30 30\n' \
+           'EnvironmentName ? 2D-forest\n'
 
     start_goal_pairs = [
         "Start ? 0 0 \nGoal ? 0 -25",
@@ -17,7 +18,8 @@ def forest_environment():
 def ladder_environment():
     base = 'EnvironmentMesh ? 3-ladder.dae\n' \
            '{start_goal_pair}\n' \
-           'EnvironmentBounds ? -15 15 -15 15\n'
+           'EnvironmentBounds ? -15 15 -15 15\n' \
+           'EnvironmentName ? ladder\n'
 
     start_goal_pairs = [
         "Start ? -13 -12 \nGoal ? -13 12",
@@ -30,12 +32,78 @@ def ladder_environment():
     return [base.format(start_goal_pair=start_goal_pair) for start_goal_pair in start_goal_pairs]
 
 
+def wall_environment():
+    base = 'EnvironmentMesh ? single-wall.dae\n' \
+           '{start_goal_pair}\n' \
+           'EnvironmentBounds ? -15 15 -15 15\n' \
+           'EnvironmentName ? single-wall\n'
+
+    start_goal_pairs = [
+        "Start ? -13 -12 \nGoal ? -13 13",
+        "Start ? -13 -13 \nGoal ? -12 10",
+        "Start ? -13 -11 \nGoal ? -10 10",
+        "Start ? -10 -12 \nGoal ? -13 13",
+        "Start ? -5 -12 \nGoal ? -13 13",
+    ]
+
+    return [base.format(start_goal_pair=start_goal_pair) for start_goal_pair in start_goal_pairs]
+
+
+def empty_environment():
+    base = 'EnvironmentMesh ? single-wall.dae\n' \
+           '{start_goal_pair}\n' \
+           'EnvironmentBounds ? -15 15 -10 15\n' \
+           'EnvironmentName ? empty-area\n'
+
+    start_goal_pairs = [
+        "Start ? -13 7 \nGoal ? 13 7",
+        "Start ? -13 8 \nGoal ? 12 8",
+        "Start ? -12 7 \nGoal ? 10 7",
+        "Start ? -10 5 \nGoal ? 10 6",
+        "Start ? -7 7 \nGoal ? 7 8",
+    ]
+
+    return [base.format(start_goal_pair=start_goal_pair) for start_goal_pair in start_goal_pairs]
+
+def threed_forest_environment():
+    base = 'EnvironmentMesh ? forest.dae\n' \
+           '{start_goal_pair}\n' \
+           'EnvironmentBounds ? -30 30 -30 30 -5 16\n' \
+           'EnvironmentName ? 3D-forest\n'
+
+    start_goal_pairs = [
+        "Start ? 0 0 15 \nGoal ? 0 -25 0",
+        "Start ? 0 0 15 \nGoal ? 0 -26 0",
+        "Start ? 0 0 15 \nGoal ? 0 -26 -1",
+        "Start ? 0 0 15 \nGoal ? 1 -25 1",
+        "Start ? 0 0 15 \nGoal ? 5 -25 -3"
+    ]
+
+    return [base.format(start_goal_pair=start_goal_pair) for start_goal_pair in start_goal_pairs]
+
+
+def threed_fifthelement_environment():
+    base = 'EnvironmentMesh ? fifthelement.dae\n' \
+           '{start_goal_pair}\n' \
+           'EnvironmentBounds ? -30 30 -30 30 0 15\n' \
+           'EnvironmentName ? fifthelement\n'
+
+    start_goal_pairs = [
+        "Start ? 15 7 5 \nGoal ? -15 7 5",
+        "Start ? 15 -7 5\nGoal ? -15 -7 5",
+        "Start ? 15 -7 5\nGoal ? -15 7 5",
+        "Start ? 15 7 5\nGoal ? -15 -7 5",
+        "Start ? 7 15 5\nGoal ? -7 -15 5",
+    ]
+
+    return [base.format(start_goal_pair=start_goal_pair) for start_goal_pair in start_goal_pairs]
+
 def dynamic_car_domain():
     base = "Domain ? DynamicCar\n" \
            "AgentMesh ? car2_planar_robot.dae\n" \
            "{environment}\n"
 
-    environments = forest_environment() + ladder_environment()
+    environments = forest_environment() + ladder_environment() + empty_environment()
 
     return [base.format(environment=environment) for environment in environments]
 
@@ -45,7 +113,37 @@ def kinematic_car_domain():
            "AgentMesh ? car2_planar_robot.dae\n" \
            "{environment}\n"
 
-    environments = forest_environment() + ladder_environment()
+    environments = forest_environment() + ladder_environment() + empty_environment()
+
+    return [base.format(environment=environment) for environment in environments]
+
+
+def hovercraft_domain():
+    base = "Domain ? Hovercraft\n" \
+           "AgentMesh ? car2_planar_robot.dae\n" \
+           "{environment}\n"
+
+    environments = forest_environment() + ladder_environment() + empty_environment()
+
+    return [base.format(environment=environment) for environment in environments]
+
+
+def quadrotor_domain():
+    base = "Domain ? Quadrotor\n" \
+           "AgentMesh ? quadrotor.dae\n" \
+           "{environment}\n"
+
+    environments = threed_forest_environment() + threed_fifthelement_environment()
+
+    return [base.format(environment=environment) for environment in environments]
+
+
+def blimp_domain():
+    base = "Domain ? Blimp\n" \
+           "AgentMesh ? blimp.dae\n" \
+           "{environment}\n"
+
+    environments = threed_forest_environment() + threed_fifthelement_environment()
 
     return [base.format(environment=environment) for environment in environments]
 
@@ -54,8 +152,12 @@ def seeded_domains():
     base = "{domain}\n" \
            "Seed ? {seed}"
 
-    domains = dynamic_car_domain() + kinematic_car_domain()
-    seeds = range(25)
+    #domains = dynamic_car_domain() + kinematic_car_domain() + \
+    #      hovercraft_domain() + quadrotor_domain() + blimp_domain()
+
+    domains= hovercraft_domain() + quadrotor_domain() + blimp_domain()
+
+    seeds = range(5)
 
     seeded_domains = []
     for domain in domains:
@@ -70,23 +172,135 @@ def planners():
     #        "WhichSearch ? D*\n"
     #
     # algorithms = ['BEAST', 'BEAST']
-    beast = "Planner ? BEAST\n" \
+    # beast = "Planner ? BEAST\n" \
+    #         "AbstractionType ? PRM\n" \
+    #         "WhichSearch ? D*\n" \
+    #         'ValidEdgeDistributionAlpha ? 10\n' \
+    #         'ValidEdgeDistributionBeta ? 1\n' \
+    #         'InvalidEdgeDistributionAlpha ? 1\n' \
+    #         'InvalidEdgeDistributionBeta ? 10\n' \
+    #         "Algorithm ? BEAST"
+
+    # alloneone = "Planner ? BEAST\n" \
+    #                   "AbstractionType ? PRM\n" \
+    #                   "WhichSearch ? D*\n" \
+    #                   'ValidEdgeDistributionAlpha ? 1\n' \
+    #                   'ValidEdgeDistributionBeta ? 1\n' \
+    #                   'InvalidEdgeDistributionAlpha ? 1\n' \
+    #                   'InvalidEdgeDistributionBeta ? 1\n' \
+    #                   "Algorithm ? AllOneOne"
+
+    # nogeometric = "Planner ? BEAST\n" \
+    #                   "AbstractionType ? PRM\n" \
+    #                   "WhichSearch ? D*NOGEOMETRIC\n" \
+    #                   'ValidEdgeDistributionAlpha ? 1\n' \
+    #                   'ValidEdgeDistributionBeta ? 1\n' \
+    #                   'InvalidEdgeDistributionAlpha ? 1\n' \
+    #                   'InvalidEdgeDistributionBeta ? 1\n' \
+    #                   "Algorithm ? AllOneOne"
+
+    # halton = "Planner ? BEAST\n" \
+    #          "AbstractionType ? PRM_HALTON\n" \
+    #          "WhichSearch ? D*NOGEOMETRIC\n" \
+    #          'ValidEdgeDistributionAlpha ? 1\n' \
+    #          'ValidEdgeDistributionBeta ? 1\n' \
+    #          'InvalidEdgeDistributionAlpha ? 1\n' \
+    #          'InvalidEdgeDistributionBeta ? 1\n' \
+    #          "Algorithm ? Halton"
+
+    # newbonus = "Planner ? BEAST\n" \
+    #            "AbstractionType ? PRM\n" \
+    #            "WhichSearch ? D*BONUS\n" \
+    #            'ValidEdgeDistributionAlpha ? 1\n' \
+    #            'ValidEdgeDistributionBeta ? 1\n' \
+    #            'InvalidEdgeDistributionAlpha ? 1\n' \
+    #            'InvalidEdgeDistributionBeta ? 1\n' \
+    #            "Algorithm ? NewBonus"
+
+    # halton_nb = "Planner ? BEAST\n" \
+    #             "AbstractionType ? PRM_HALTON\n" \
+    #             "WhichSearch ? D*BONUS\n" \
+    #             'ValidEdgeDistributionAlpha ? 1\n' \
+    #             'ValidEdgeDistributionBeta ? 1\n' \
+    #             'InvalidEdgeDistributionAlpha ? 1\n' \
+    #             'InvalidEdgeDistributionBeta ? 1\n' \
+    #             "Algorithm ? HaltonNB"
+
+    #gust = "Planner ? GUST\n" \
+    #       "AbstractionType ? PRM_GUST\n" \
+    #       "WhichSearch ? D*\n" \
+    #       'ValidEdgeDistributionAlpha ? 10\n' \
+    #       'ValidEdgeDistributionBeta ? 1\n' \
+    #       'InvalidEdgeDistributionAlpha ? 1\n' \
+    #       'InvalidEdgeDistributionBeta ? 10\n' \
+    #       'Alpha ? 8\n' \
+    #       'B ? 0.85\n' \
+    #       'Beta ? 0.85\n' \
+    #       'Delta ? 0.15\n' \
+    #       'UseSplit ? 1\n' \
+    #       'Algorithm ? GUST'
+    beast_int_b0 = "Planner ? BEAST\n" \
             "AbstractionType ? PRM\n" \
-            "WhichSearch ? D*\n" \
-            "Algorithm ? BEAST"
+            "WhichSearch ? Integrated\n" \
+            'ValidEdgeDistributionAlpha ? 10\n' \
+            'ValidEdgeDistributionBeta ? 1\n' \
+            'InvalidEdgeDistributionAlpha ? 1\n' \
+            'InvalidEdgeDistributionBeta ? 10\n' \
+            "Algorithm ? BEAST_INT\n" \
+            "RegionCount ? 1000\n" \
+            "InitialAlpha ? 1\n" \
+            "InitialBeta ? 1\n" \
+            "BonusType ? 0\n" 
+    beast_int_b1 = "Planner ? BEAST\n" \
+            "AbstractionType ? PRM\n" \
+            "WhichSearch ? Integrated\n" \
+            'ValidEdgeDistributionAlpha ? 10\n' \
+            'ValidEdgeDistributionBeta ? 1\n' \
+            'InvalidEdgeDistributionAlpha ? 1\n' \
+            'InvalidEdgeDistributionBeta ? 10\n' \
+            "Algorithm ? BEAST_INT\n" \
+            "RegionCount ? 1000\n" \
+            "InitialAlpha ? 1\n" \
+            "InitialBeta ? 1\n" \
+            "BonusType ? 1\n" 
+    beast_int_b2 = "Planner ? BEAST\n" \
+            "AbstractionType ? PRM\n" \
+            "WhichSearch ? Integrated\n" \
+            'ValidEdgeDistributionAlpha ? 10\n' \
+            'ValidEdgeDistributionBeta ? 1\n' \
+            'InvalidEdgeDistributionAlpha ? 1\n' \
+            'InvalidEdgeDistributionBeta ? 10\n' \
+            "Algorithm ? BEAST_INT\n" \
+            "RegionCount ? 1000\n" \
+            "InitialAlpha ? 1\n" \
+            "InitialBeta ? 1\n" \
+            "BonusType ? 2\n" 
+    beast_int_b3 = "Planner ? BEAST\n" \
+            "AbstractionType ? PRM\n" \
+            "WhichSearch ? Integrated\n" \
+            'ValidEdgeDistributionAlpha ? 10\n' \
+            'ValidEdgeDistributionBeta ? 1\n' \
+            'InvalidEdgeDistributionAlpha ? 1\n' \
+            'InvalidEdgeDistributionBeta ? 10\n' \
+            "Algorithm ? BEAST_INT\n" \
+            "RegionCount ? 1000\n" \
+            "InitialAlpha ? 1\n" \
+            "InitialBeta ? 1\n" \
+            "BonusType ? 3\n" 
+    #beast_slo = "Planner ? BEAST\n" \
+    #        "AbstractionType ? PRM\n" \
+    #        "WhichSearch ? D*\n" \
+    #        'ValidEdgeDistributionAlpha ? 10\n' \
+    #        'ValidEdgeDistributionBeta ? 1\n' \
+    #        'InvalidEdgeDistributionAlpha ? 1\n' \
+    #        'InvalidEdgeDistributionBeta ? 10\n' \
+    #        "Algorithm ? BEASTSLOW"
 
-    halton = "Planner ? BEAST\n" \
-             "AbstractionType ? PRM_HALTON\n" \
-             "WhichSearch ? D*\n" \
-             "Algorithm ? Halton"
 
-    newbonus = "Planner ? BEAST\n" \
-               "AbstractionType ? PRM\n" \
-               "WhichSearch ? D*BONUS\n" \
-               "Algorithm ? NewBonus"
 
     # return [base.format(planner=planner) for planner in algorithms] + [halton]
-    return [halton, newbonus, beast]
+#return [nogeometric, halton, newbonus, beast]
+    return [beast_int_b0, beast_int_b1, beast_int_b2, beast_int_b3]
 
 
 def generate_configurations():
@@ -103,10 +317,8 @@ def generate_configurations():
            'NumEdges ? 5\n' \
            'PRMSize ? 1000\n' \
            'StateRadius ? 6\n' \
-           'ValidEdgeDistributionAlpha ? 10\n' \
-           'ValidEdgeDistributionBeta ? 1\n' \
-           'InvalidEdgeDistributionAlpha ? 1\n' \
-           'InvalidEdgeDistributionBeta ? 10\n'
+           'PRMResizeFactor ? 2\n' \
+           
 
     algorithms = planners()
     domains = seeded_domains()
